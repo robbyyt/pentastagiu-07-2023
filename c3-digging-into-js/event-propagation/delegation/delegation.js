07d5bca73fbe3fc964a5519c94d97f70cf5b2ee2
Robert Tabacaru <49028058+robbyyt@users.noreply.github.com>
2023-08-14 20:23:26 +0300

    delete stuff from c3

--- c3-digging-into-js/event-propagation/delegation/delegation.js
+++
@@ -1,9 +0,0 @@
-const taskList = document.getElementById("taskList");
-
-// With delegation, we can add an event listener at parent level
-// which will capture and handle the event from its children
-taskList.addEventListener("click", function (event) {
-  if (event.target.id === "delegation") {
-    console.log(`Task "${event.target.textContent}" clicked`);
-  }
-});
